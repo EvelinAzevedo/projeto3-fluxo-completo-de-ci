9267d9f4fe68901d5d34b30f3933b96b16761ceb
Evelin✨ <evelinmariabarroso@gmail.com>
2026-05-27 01:18:51 +0000

    feature: adicionara o calculo de subtracao

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-const { soma } = require('./calculadora')
+const { soma, subtracao } = require('./calculadora')
 
 const resultadoSoma = soma(2, 4)
 
+const resultadoSubtracao = subtracao(4, 2)
+
 console.log(`
     soma de 2 + 4 = ${resultadoSoma}
+    subtração de 4 - 2 = ${resultadoSubtracao}
     `)
